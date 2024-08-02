dc28b116769088ab6143e02cfe105635fbf81101
JulesCtn <97630880+JulesCtn@users.noreply.github.com>
2024-08-02 09:59:22 +0200

    Sim works, but not logic analyzer

--- modules/step_generator.vhd
+++ modules/step_generator.vhd
@@ -20,66 +20,64 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity step_generator is
     generic(
-		constant DATA_BITS:		integer := 32;		-- Number of bits to support ARR_MAX
 		constant PULSE_WIDTH:	integer := 2; 		-- Width of one pulse (in µs)
-		constant FREQ_FPGA:		integer := 400; 	-- Internal FPGA frequency (in MHz)
-		constant N_POINT:		integer := 1		-- Number of point for interpolation
+		constant FREQ_FPGA:		integer := 400 		-- Internal FPGA frequency (in MHz)
         );
     Port(
 		cpt_clk_i:		in std_logic;
 		reset_n_i:		in std_logic;
-		arr_i:			in integer; --std_logic_vector(DATA_BITS-1 downto 0);
+		arr_i:			in integer;
 		step_o:			out std_logic
         );
 end step_generator;
 
 architecture Behavioral of step_generator is
 -- Constant
-	constant ARR_MIN: integer := FREQ_FPGA*PULSE_WIDTH; -- A step lasts from ARR_MIN to 0
+	constant ARR_MIN: integer := FREQ_FPGA * PULSE_WIDTH; -- A step lasts from ARR_MIN to 0
 -- Signals
 	signal counter:			integer := 0;
-	signal desired_arr:		integer; -- ARR(n)
-	signal next_arr:		integer; -- ARR(n+1)
-	signal nb_point:		std_logic;
-	signal debug_calculated_arr:	integer;	-- Debugg signal
+	signal nb_point:		std_logic := '0';
+	signal dbug_nxt_arr:	std_logic_vector(26 downto 0); 	-- Debugg signal (optional)
+	signal dbug_curr_arr:	std_logic_vector(26 downto 0);	-- Debugg signal (optional)
+	signal old_arr:			integer := 0;
 begin
 -- Down counting process
 	down_cpt_proc : process(cpt_clk_i, reset_n_i)
-	variable interpolated_arr:	integer;
+	variable interpolated_arr:	integer := 0;
+	variable next_arr:			integer := 0; -- ARR(n+1)
 	begin
 		if reset_n_i = '0' then
 			counter <= 0;
-			desired_arr <= 0;
-			next_arr <= 0;
+			old_arr <= 0;
+			next_arr := 0;
 			interpolated_arr := 0;
 			nb_point <= '0';
 			step_o <= '0';
 		elsif rising_edge(cpt_clk_i) then
 			if counter = ARR_MIN then
 				step_o <= '1';
-				counter <= counter -1;
-			elsif counter = 0 then
+				counter <= counter - 1;
+			elsif counter <= 1 then
 				step_o <= '0';
-			-- If 1st point: interpolated point
-				if nb_point = '0' then	
-					next_arr <= arr_i;
-					desired_arr <= next_arr;
-					interpolated_arr := desired_arr + ((next_arr - desired_arr)/2); -- ARR = ARR0 + (ARR1 - ARR0)/?
+				-- If 1st point -> interpolated point
+				if nb_point = '0' then
+					next_arr := arr_i;
+					interpolated_arr := old_arr + ((next_arr - old_arr)/2); -- ARR = ARR0 + (ARR1 - ARR0)/2
 					counter <= interpolated_arr;
 					nb_point <= '1';
-					debug_calculated_arr <= interpolated_arr;	-- Debugg signal
-			-- Si 2nd point: goal point
-				else	
-					next_arr <= arr_i;
-					desired_arr <= next_arr;
-					counter <= desired_arr;
+					dbug_curr_arr <= std_logic_vector(to_unsigned(interpolated_arr, 27));	-- Debugg signal (optional)
+				-- If 2nd point -> goal point
+				else
+					old_arr <= next_arr;
+					counter <= next_arr;
 					nb_point <= '0';
-					debug_calculated_arr <= desired_arr;	-- Debugg signal
+					dbug_curr_arr <= std_logic_vector(to_unsigned(next_arr, 27));	-- Debugg signal (optional)
 				end if;
-				
 			else
 				counter <= counter -1;
 			end if;
 		end if;
+		dbug_nxt_arr <= std_logic_vector(to_unsigned(next_arr, 27));	-- Debugg signal (optional)
 	end process;
+	
 end Behavioral;
